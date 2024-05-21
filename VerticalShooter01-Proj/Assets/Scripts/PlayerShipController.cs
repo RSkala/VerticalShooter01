a9bb0d2b7d276c0f2df5073b1ded6f8de6a0127f
RSkala <RichardSkala@gmail.com>
2024-05-20 21:23:45 -0700

    experimenting with power up states

--- VerticalShooter01-Proj/Assets/Scripts/PlayerShipController.cs
+++ VerticalShooter01-Proj/Assets/Scripts/PlayerShipController.cs
@@ -11,9 +11,22 @@ public class PlayerShipController : MonoBehaviour
     Rigidbody2D _rigidbody2D;
     Vector2 _movementInput;
 
+    enum PowerUpState
+    {
+        Level_0 = 0, // Double Straight Fire (Default, No PowerUps)
+        Level_1 = 1, // Double Straight Fire + 1 Backwards Fire
+        Level_2 = 2, // Single Straight Fire + 2 Angle Fire + 1 Backwards Fire
+        Level_3 = 3, // Single Straight Fire + 2 Angle Fire + 2 Backwards Fire
+        Level_4 = 4, // Double Straight Fire + 2 Angle Fire + 2 Backwards Fire
+        NumPowerUpStates = 5
+    }
+
+    PowerUpState _currentPowerUpState;
+
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _currentPowerUpState = PowerUpState.Level_0;
     }
 
     void Update()
@@ -32,13 +45,45 @@ public class PlayerShipController : MonoBehaviour
 
     void OnFire(InputValue inputValue)
     {
-        Debug.Log("OnFire");
-
         GameObject.Instantiate<Projectile>(_projectilePrefab, _rigidbody2D.position, Quaternion.identity);
     }
 
     void OnMove(InputValue inputValue)
     {
         _movementInput = inputValue.Get<Vector2>();
+    }
+
+    void OnIncreasePowerUpState()
+    {
+        Debug.Log("OnIncreasePowerUpState");
+        IncrementPowerUpState();
+        Debug.Log(" -> _currentPowerUpState: " + _currentPowerUpState);
+    }
+
+    void OnDecreasePowerUpState()
+    {
+        Debug.Log("OnDecreasePowerUpState");
+        DecrementPowerUpState();
+        Debug.Log(" -> _currentPowerUpState: " + _currentPowerUpState);
+    }
+
+    void IncrementPowerUpState()
+    {
+        int nextPowerUpState = (int)_currentPowerUpState + 1;
+        if(nextPowerUpState >= (int)PowerUpState.NumPowerUpStates)
+        {
+            nextPowerUpState = (int)PowerUpState.Level_4;
+        }
+        _currentPowerUpState = (PowerUpState)nextPowerUpState;
+    }
+
+    void DecrementPowerUpState()
+    {
+        int nextPowerUpState = (int)_currentPowerUpState - 1;
+        if (nextPowerUpState <= (int)PowerUpState.Level_0)
+        {
+            nextPowerUpState = (int)PowerUpState.Level_0;
+        }
+        _currentPowerUpState = (PowerUpState)nextPowerUpState;
     }
 }
