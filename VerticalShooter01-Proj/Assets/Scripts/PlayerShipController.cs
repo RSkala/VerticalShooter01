82874eac2c649ee5925c9c4a6148fc97770d29ec
RSkala <RichardSkala@gmail.com>
2024-05-20 19:07:53 -0700

    Added input actions

---
+++ VerticalShooter01-Proj/Assets/Scripts/PlayerShipController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShipController : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("PlayerShipController.Start");
+    }
+
+    void Update()
+    {
+        Debug.Log("PlayerShipController.Update");
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
